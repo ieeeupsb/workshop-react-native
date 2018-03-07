9291c7ad9627ab20c0b9a0ae920b6a818d6e9305
Areadrill <jackthemotorcycle@gmail.com>
2018-03-07 04:55:30 +0000

    removing accidental deployment files

--- server/dist/templates/room.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Room {
-    constructor(name, creator) {
-        this.name = name;
-        this.creator = creator;
-    }
-}
-exports.default = Room;
